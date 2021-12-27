c4374a5373e0bc17faafce8d1c292ef6940fb9f3
Simon Hammes <simonhammes@users.noreply.github.com>
2021-12-27 15:15:51 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM php:7.4-apache-buster
+
+ENV ACCEPT_EULA="Y"
+
+RUN apt-get update && \
+    apt-get install -y gnupg && \
+    curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+    curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
+    apt-get update -y && \
+    apt-get install -y msodbcsql17 unixodbc-dev && \
+    pecl install sqlsrv-5.9.0 && \
+    docker-php-ext-enable sqlsrv
+
+RUN php -m
---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$connection = sqlsrv_connect('db', ['Database' => 'master', 'UID' => 'sa', 'PWD' => 'MySuperSecurePassword1!']);
+
+// sqlsrv_query($connection, "CREATE TABLE users (id INT NOT NULL, name VARCHAR(80) NOT NULL, time DATETIME NOT NULL);");
+
+sqlsrv_query($connection, 'INSERT INTO users (id, name, time) VALUES (?, ?, ?)', [2, "John Doe", "2021-01-01 12:34:56"]);
+
+$result = sqlsrv_query($connection, "SELECT * FROM users", [], ['Scrollable' => SQLSRV_CURSOR_STATIC]);
+
+$row_count = sqlsrv_num_rows($result);
+
+echo "Number of rows: $row_count . <br><br>";
+
+while ( $row =  sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC) ) {
+    print_r( $row );
+    echo '<br>';
+}
+
+
+
+// var_dump( $rows );
