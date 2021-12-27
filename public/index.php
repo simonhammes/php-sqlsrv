017c8ec78d03051c23a54f88556886622dbc3fa3
Simon Hammes <simonhammes@users.noreply.github.com>
2021-12-27 19:45:29 +0100

    Add GitHub Actions workflow

--- public/index.php
+++ public/index.php
@@ -2,11 +2,15 @@
 
 $connection = sqlsrv_connect('db', ['Database' => 'master', 'UID' => 'sa', 'PWD' => 'MySuperSecurePassword1!']);
 
-// sqlsrv_query($connection, "CREATE TABLE users (id INT NOT NULL, name VARCHAR(80) NOT NULL, time DATETIME NOT NULL);");
+sqlsrv_query($connection, "CREATE TABLE users (id INT NOT NULL, name VARCHAR(80) NOT NULL, time DATETIME NOT NULL);");
 
 sqlsrv_query($connection, 'INSERT INTO users (id, name, time) VALUES (?, ?, ?)', [2, "John Doe", "2021-01-01 12:34:56"]);
 
 $result = sqlsrv_query($connection, "SELECT * FROM users", [], ['Scrollable' => SQLSRV_CURSOR_STATIC]);
+
+if ( $result === FALSE ) {
+    exit(1);
+}
 
 $row_count = sqlsrv_num_rows($result);
 
